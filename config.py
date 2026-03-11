d702bf266e02f9437f8d2e571778091a52eeba6b
unknown <bobylevrlax@gmail.com>
2026-03-11 14:56:39 +0500

    Update bot, add files handling and webhook

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "ВАШ_TELEGRAM_BOT_TOKEN"
-BOT_ADMIN_PASSWORD = "ВАШ_TELEGRAM_ADMIN_ID"
-APP_URL = "https://YOUR_APP.onrender.com"
+BOT_TOKEN = "8263077456:AAHX26xlcozhQ_pdCYcs7Gd0lVw5JeuKUP0"
+BOT_ADMIN_PASSWORD = "8318264587"
+APP_URL = "https://shooo-uceo.onrender.com"
